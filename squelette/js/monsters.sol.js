6c073033ff0ae55bb9d4cc5f766b84b99fdadb9c
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2016-11-01 13:16:34 +0100

    fonction exécutant toutes les 12s une action aléatoire

--- squelette/js/monsters.sol.js
+++ squelette/js/monsters.sol.js
@@ -4,35 +4,35 @@ var monster = {
 };
 
 monster.modules.actions = (function(){
-	
+
 	var name = "George";
 	var life = 70;
 	var money = 200;
 	var awake = true;
-	
+
 	return {
-		
+
 		showme: function(){
-			
+
 			if(awake === true){
 				awake = "awake";
 			}else{
 				awake = "sleeping";
 			}
-			
+
 			monster.modules.app.displayStatus(life, money, awake);
-			
+
 			monster.modules.app.log("Le monstre s'appelle "+name+", il a "+life+" de points de vie et "+money+" en argent. Il est actuellement "+awake);
-			
+
 		},
-		
+
 		init: function(n, l, m, a){
 			name = n;
 			life = l;
 			money = m;
 			awake = a;
 		},
-		
+
 		run: function(){
 			if(life >= 1 && awake === true){
 				monster.modules.app.log("The monster is running.");
@@ -40,7 +40,7 @@ monster.modules.actions = (function(){
 				monster.modules.app.displayStatus(life, money, awake);
 			}
 		},
-		
+
 		fight: function(){
 			if(life >= 3 && awake === true){
 				monster.modules.app.log("The monster is fighting.");
@@ -48,7 +48,7 @@ monster.modules.actions = (function(){
 				monster.modules.app.displayStatus(life, money, awake);
 			}
 		},
-		
+
 		work: function(){
 			if(life >= 1 && awake === true){
 				monster.modules.app.log("The monster is working.");
@@ -57,7 +57,7 @@ monster.modules.actions = (function(){
 				monster.modules.app.displayStatus(life, money, awake);
 			}
 		},
-		
+
 		eat: function(){
 			if(money >= 3 && awake === true){
 				monster.modules.app.log("The monster is eating.");
@@ -66,9 +66,9 @@ monster.modules.actions = (function(){
 				monster.modules.app.displayStatus(life, money, awake);
 			}
 		},
-		
+
 		sleep: function(){
-			
+
 			monster.modules.app.log("The monster is sleeping.");
 			awake = false;
 			monster.modules.app.displayStatus(life, money, "sleeping");
@@ -78,85 +78,91 @@ monster.modules.actions = (function(){
 				life = life+1;
 				monster.modules.app.displayStatus(life, money, "awake");
 			}, 10000);
-			
+
 		}
-		
+
 	}
-	
+
 })();
 
 monster.modules.app = (function(){
-	
+
 	return {
-		
+
 		run: function(){
-	
+
 			var run = document.getElementById("b2");
 			var fight = document.getElementById("b3");
 			var work = document.getElementById("b7");
 			var sleep = document.getElementById("b4");
 			var eat = document.getElementById("b5");
 			var show = document.getElementById("b6");
-	
+
 			monster.modules.actions.init("Fred", 80, 180, true);
 			show.onclick = monster.modules.actions.showme;
-			
+
 			run.onclick = monster.modules.actions.run;
 			fight.onclick = monster.modules.actions.fight;
 			work.onclick = monster.modules.actions.work;
 			eat.onclick = monster.modules.actions.eat;
 			sleep.onclick = monster.modules.actions.sleep;
-			
+
+			setInterval(function(){
+				var action = Math.floor(Math.random() * 5 + 1);
+				if(action = 1){
+					monster.modules.actions.run;
+				}else if(action = 2){
+					monster.modules.actions.fight;
+				}else if(action = 3){
+					monster.modules.actions.work;
+				}else if(action = 4){
+					monster.modules.actions.eat;
+				}else if(action = 5){
+					monster.modules.actions.sleep;
+				}
+			}, 12000);
+
 		},
-		
+
 		log: (function(message){
-			
+
 			var p = document.createElement("p");
 			var p_content = document.createTextNode(message);
-			
+
 			p.appendChild(p_content);
-			
+
 			var box = document.querySelector("#actionbox p");
 			var box_parent = box.parentNode;
 			box_parent.insertBefore(p, box);
-			
+
 		}),
-		
+
 		displayStatus: (function(life, money, awake){
-			
+
 			var parent = document.getElementById("status");
-			
+
 			var li_1 = document.createElement("li");
 			var li_1_content = document.createTextNode("life:"+life);
 			li_1.appendChild(li_1_content);
 			var li_1_init = document.querySelectorAll("#status li")[0];
 			parent.replaceChild(li_1, li_1_init);
-			
+
 			var li_2 = document.createElement("li");
 			var li_2_content = document.createTextNode("money:"+money);
 			li_2.appendChild(li_2_content);
 			var li_2_init = document.querySelectorAll("#status li")[1];
 			parent.replaceChild(li_2, li_2_init);
-			
+
 			var li_3 = document.createElement("li");
 			var li_3_content = document.createTextNode(awake);
 			li_3.appendChild(li_3_content);
 			var li_3_init = document.querySelectorAll("#status li")[2];
 			parent.replaceChild(li_3, li_3_init);
-			
+
 		})
-		
+
 	}
-	
+
 })();
 
 window.onload = monster.modules.app.run();
-
-setInterval(function(){
-	
-}, 12000);
-
-
-
-
-
